edbbdc9275d4e3e3bf6e72d77e32c88e5a3aeefc
Nisha Yadav <nisha@songdew.com>
2024-07-12 01:24:04 +0530

    code

--- server/index.js
+++ server/index.js
@@ -14,6 +14,16 @@ const app = express();
 app.use(express.json({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({ credentials: true, origin: 'https://geeks-blog.onrender.com' }))
+app.use(function (req, res, next) {
+    const allowedOrigins = ['http://localhost:3000', 'https://geeks-blog.onrender.com', 'https://geeks-blog.onrender.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+});
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
