dd5ddd5417dd57f4a4aaa814ba180fe5e2c026aa
Nisha Yadav <nisha@songdew.com>
2024-07-12 01:44:38 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -7,23 +7,18 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleWare')
 const { connect } = require('mongoose')
 const upload = require('express-fileupload')
 
-
+const corsOptions = {
+    origin: 'https://geeks-blog.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
 
 
 const app = express();
 app.use(express.json({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({ credentials: true, origin: 'https://geeks-blog.onrender.com' }))
-app.use(function (req, res, next) {
-    const allowedOrigins = ['http://localhost:3000', 'https://geeks-blog.onrender.com', 'https://geeks-blog.onrender.com'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-});
+app.use(cors(corsOptions))
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
